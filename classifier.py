1f0390827d2b1b51e6784a2d545309fe40bbe4e9
Philippenko <Philippenko@Evariste-Galois>
2017-06-30 21:15:52 +0300

    classifier clean

--- classifier.py
+++ classifier.py
@@ -20,8 +20,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import load as ld
 from comparaison import build_distance_vector
-from computation import normalization
-from variables import activities
+from variables import activities, n_act
 from time import time
 
 classifiers=[svm.SVC(),GaussianNB(),neighbors.KNeighborsClassifier(),tree.DecisionTreeClassifier(min_samples_leaf=5,max_depth=15)]
@@ -32,26 +31,29 @@ def compute_data(category="manual"):
     
     Parameter
     ----------
-    catergorie: string-like
-        where to take the data, for instance : 
+    categorie: string-like
+        where to take the data, for instance : "manual", "test", "automatic"
+        
+    Return 
+    ------
+    (X,y): numpy.array-like
+        the data and the associated label
     """
+    
     start = time()
     X=[]
     y=[]
-    for i in range(0,7):    
+    for i in range(0,n_act):    
         print(activities[i])
         serie_path=ld.get_filename(activities[i], category)
         serie=ld.load_serie(serie_path)
         sgmtt=ld.load_segmentation(serie_path)
         bp=sgmtt.get_breaking_points()
         print("Computing distance characterization of the series ...")
-        for k in range(len(bp)-1):
+        for k in range(0,len(bp)-1):
             print("Number : ",k)
             X.append(build_distance_vector(bp[k],serie))
             y.append(i)
-#         for k in range(len(bp)-2):
-#             X.append(distance_optimum(template, template_variance, normalization(serie[bp[k]+longueur_fenetre/2:bp[k]+3*longueur_fenetre/2])))
-#             y.append(0)
     print("X=",X)
     print("Y=",y)
     end = time()
@@ -126,10 +128,25 @@ def plot_data_components_i_j(X,y,i,j, x_label, y_label,save=False):
     else:
         plt.show()
     
+    
 
 def performance_classifier(train_data, train_label, test_data, test_label, label_predicted):
     """
     Measures the performance of a prediction
+    
+    Parameters
+    ----------
+    train_data, train_label: numpy.array-like
+        the train set
+    test_data, test_label: numpy.array-like
+        the test set
+    label_predicted: numpy.array-like
+        the predicted label of the test set w.r.t to the training
+        
+    Return 
+    ------
+    confusion_matrix: matrix of type numpy.array, size 
+        The confusion matrix of the classifier
     """
     print("True Test Label : ")
     print(test_label)
@@ -137,27 +154,36 @@ def performance_classifier(train_data, train_label, test_data, test_label, label
     print(label_predicted)
     exact=[test_label[i] for i in range(len(test_label)) 
            if test_label[i]==label_predicted[i]] 
-    total=[] 
+    confusion_matrix=[] 
     print("Erreur Globale: ", (1-float(len(exact))/len(test_label))*100,"%")
     # For each class we will look what we have recognized
-    for i in range(len(activities[0:7])):
+    for i in range(n_act):
         under_total=[]
         # For each activities, we look how many we have recognized it.
-        for j in range(len(activities[0:7])):
+        for j in range(n_act):
             under_total.append(recognition_rate_of_j_when_i(i,j,test_label,label_predicted))
-        total.append(under_total)
+        confusion_matrix.append(under_total)
             
-    return np.array(total)
+    return np.array(confusion_matrix)
 
-def recognition_rate_of_j_when_i(i,j,test,predicted):
+def recognition_rate_of_j_when_i(i,j,label_test,label_predicted):
     """
-    Measures the rate of element which have been labeled with j when it should have been labeled with i.
+    Measures the rate of the elements which have been labeled with j when they should have been labeled with i.
     Perfect performances:
         - i == j => return 100
         - i != j => return 0
+        
+    Parameter:
+    ----------
+    i,j: int-like
+        the number of the class
+    label_test:  numpy.array-like
+        the labels of the test data
+    label_predicted: numpy.array-like
+        the predicted labels
     """
-    n=sum([t==i for t in test])
-    return float(sum([test[k]==i and predicted[k]==j for k in range(len(test))])*100)/n
+    n=sum([t==i for t in label_test])
+    return float(sum([label_test[k]==i and label_predicted[k]==j for k in range(len(label_test))])*100)/n
 
 def test_performance_clf(X,x,Y,y,predicted,title):
     """
@@ -165,11 +191,16 @@ def test_performance_clf(X,x,Y,y,predicted,title):
     
     Parameters
     ----------
-    X: training data
-    x: training labels
-    Y: test data
-    y: test labels
-    predicted: the label predicted with the classifier
+    X: matrix of numpy.array-like 
+        training data
+    x: numpy.array-like
+        training labels
+    Y: matrix of numpy.array-like 
+        test data
+    y: numpy.array-like
+        test labels
+    predicted: numpy.array-like
+        the label predicted with the classifier
     title: string
         the title of the graph
     """
@@ -177,8 +208,13 @@ def test_performance_clf(X,x,Y,y,predicted,title):
     np.savetxt('data\\rate.txt', rate, delimiter=',')
     plt.matshow(rate)
     plt.colorbar()
-    plt.xlabel("Predicted")
-    plt.ylabel("True")
+    plt.title("Confusion matrix")
+    tick_marks = np.arange(n_act)
+    plt.xticks(tick_marks, activities[:n_act], rotation=90)
+    plt.yticks(tick_marks, activities[:n_act])
+    plt.tight_layout()
+    plt.xlabel("Predicted label")
+    plt.ylabel("True label")
     plt.title(title)
     plt.show()
     print(rate)
@@ -189,14 +225,13 @@ def test_performance():
     Performs a test of the classifiers performances.
     For each of our classifier, carries out an auto-recognition test and an hold-out test.
     """
-    X,x=compute_data("manual")
-    Y,y=compute_data("test")
+    X_train,y_train=compute_data("manual")
+    X_test,y_test=compute_data("test")
     for clf in classifiers:
-        clf.fit(X, x)
-        predicted_labels=clf.predict(X)
-        test_performance_clf(X, x, X, x, predicted_labels, "Auto-recognition performance")
-        predicted_labels=clf.predict(Y)
-        test_performance_clf(X, x, Y, y, predicted_labels, "Hold-Out recognition performance")
+        clf.fit(X_train, y_train)
+        predicted_labels=clf.predict(X_train)
+        test_performance_clf(X_train, y_train, X_train, y_train, predicted_labels, "Auto-recognition performance")
+        predicted_labels=clf.predict(X_test)
+        test_performance_clf(X_train, y_train, X_test, y_test, predicted_labels, "Hold-Out recognition performance")
         
-    
     
